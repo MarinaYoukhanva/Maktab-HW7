d0938fc816f00a0e7e3f2ef8cc9a9e64e9a5c6e2
MarinaYoukhanva <166758761+MarinaYoukhanva@users.noreply.github.com>
2024-10-28 21:38:13 +0330

    importing joda time instead of java time for AuthorService class

--- src/main/java/service/AuthorService.java
+++ src/main/java/service/AuthorService.java
@@ -3,7 +3,7 @@ package service;
 import database.Database;
 import model.*;
 
-import java.time.LocalDateTime;
+import org.joda.time.LocalDateTime;
 
 public class AuthorService extends UserService{
 
